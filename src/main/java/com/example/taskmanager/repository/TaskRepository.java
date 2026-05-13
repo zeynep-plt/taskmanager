ab2864aa875e395f72e68188198754c240e89ac1
zeynep-plt <polat200011234@gmail.com>
2026-05-13 10:39:34 +0300

    Derived Query Methods is added to TaskRepostory

--- src/main/java/com/example/taskmanager/repository/TaskRepository.java
+++ src/main/java/com/example/taskmanager/repository/TaskRepository.java
@@ -1,6 +1,6 @@
-package com.example.taskmanager.repository;
+package org.example.taskmanager.repository;
 
-import com.example.taskmanager.entity.Task;
+import org.example.taskmanager.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
